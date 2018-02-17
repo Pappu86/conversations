ac68d54d75ca5be2135d2a41f797aac9e9bd5406
Md.Pappu Miahn <cse.pappu@gmail.com>
2018-02-17 10:07:04 +0600

    Login page design

--- conversation-client/src/index.js
+++ conversation-client/src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class FormExample extends React.Component {
+    render() {
+    return (
+      <div className="container">
+        <div className="login-container">
+          <div className="login-header">Login</div>
+          <div className="login-body">
+            <form>
+              <div className="form-group">                
+                <input type="email" className="form-control" placeholder="Enter email" />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control" placeholder="Password" />
+              </div> 
+              <div className="text-center">
+                <button type="submit" className="btn btn-success width-100P">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <FormExample />,
+  document.getElementById('root')
+);
